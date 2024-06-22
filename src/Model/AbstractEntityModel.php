3160412a3e05857f07f656baf3b1d78d168e9ed2
Vojtěch Bojda <info@vojtechbojda.cz>
2024-06-22 18:14:34 +0200

    adds AbstractEntityModel

---
+++ src/Model/AbstractEntityModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Sovic\Common\Model;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+abstract class AbstractEntityModel
+{
+    protected EntityManagerInterface $entityManager;
+    protected TranslatorInterface $translator;
+
+    public mixed $entity;
+
+    public function setEntityManager(EntityManagerInterface $entityManager): void
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    protected function getEntityManager(): EntityManagerInterface
+    {
+        return $this->entityManager;
+    }
+
+    public function setTranslator(TranslatorInterface $translator): void
+    {
+        $this->translator = $translator;
+    }
+
+    protected function flush(): void
+    {
+        $this->entityManager->persist($this->entity);
+        $this->entityManager->flush();
+    }
+
+    public function remove(): void
+    {
+        $this->entityManager->remove($this->entity);
+        $this->entityManager->flush();
+    }
+
+    public function refresh(): void
+    {
+        $this->entityManager->refresh($this->entity);
+    }
+}
