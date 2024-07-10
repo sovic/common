caf92ad6e9954f4f52a68f40fa63f086c22a1c1d
Vojtěch Bojda <info@vojtechbojda.cz>
2024-07-10 21:14:07 +0200

    updates pagination

--- src/Pagination/Pagination.php
+++ src/Pagination/Pagination.php
@@ -6,34 +6,21 @@ use InvalidArgumentException;
 
 class Pagination
 {
-    private int $total;
-    private int $perPage;
-    private int $currentPage = 1;
-
-    public function __construct(int $total, int $perPage)
-    {
-        $this->total = $total;
-        $this->perPage = $perPage;
+    public function __construct(
+        private int $total,
+        private int $limitPerPage,
+        private int $currentPage = 1,
+    ) {
     }
 
     public function getPageCount(): int
     {
-        $pagesCount = floor($this->getTotal() / $this->getPerPage());
-        if ($pagesCount * $this->getPerPage() < $this->getTotal()) {
+        $pagesCount = floor($this->getTotal() / $this->getLimitPerPage());
+        if ($pagesCount * $this->getLimitPerPage() < $this->getTotal()) {
             $pagesCount++;
         }
 
         return $pagesCount;
-    }
-
-    public function getPerPage(): int
-    {
-        return $this->perPage;
-    }
-
-    public function setPerPage(int $perPage): void
-    {
-        $this->perPage = $perPage;
     }
 
     public function getTotal(): int
@@ -46,6 +33,17 @@ class Pagination
         $this->total = $total;
     }
 
+    public function getLimitPerPage(): int
+    {
+        return $this->limitPerPage;
+    }
+
+    public function setLimitPerPage(int $limitPerPage): void
+    {
+        $this->limitPerPage = $limitPerPage;
+    }
+
+
     public function getCurrentPage(): int
     {
         return $this->currentPage;
@@ -57,7 +55,7 @@ class Pagination
             throw new InvalidArgumentException('invalid page');
         }
         // check validity as 0 based
-        if (($currentPage - 1) * $this->getPerPage() > $this->getTotal()) {
+        if (($currentPage - 1) * $this->getLimitPerPage() > $this->getTotal()) {
             throw new InvalidArgumentException('invalid page');
         }
         $this->currentPage = $currentPage;
