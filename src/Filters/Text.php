df1da243b0843ce19324d7a1359494e28d6d681c
Vojtěch Bojda <info@vojtechbojda.cz>
2024-07-12 02:59:41 +0200

    adds fixUtf8

--- src/Filters/Text.php
+++ src/Filters/Text.php
@@ -78,4 +78,15 @@ class Text
 
         return trim((string) preg_replace($searchRegex, ' $1&nbsp;', $text));
     }
+
+    /** @noinspection PhpRedundantOptionalArgumentInspection */
+    public static function fixUtf8(string $value): string
+    {
+        $encoding = mb_detect_encoding($value, mb_detect_order(), false);
+        if ($encoding === 'UTF-8') {
+            $value = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
+        }
+
+        return iconv(mb_detect_encoding($value, mb_detect_order(), false), 'UTF-8//IGNORE', $value);
+    }
 }
