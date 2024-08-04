f633b833e44db961c43779e721cfa4abaf5ea540
Vojtěch Bojda <info@vojtechbojda.cz>
2024-08-04 19:28:42 +0200

    adds personal/company id. number validator

---
+++ src/Validation/CompanyIdentificationNumberValidator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Sovic\Common\Validation;
+
+use InvalidArgumentException;
+use Sovic\Common\Entity\Enum\CountryId;
+
+class CompanyIdentificationNumberValidator
+{
+    public function validate(string $value, CountryId $countryId): bool
+    {
+        return match ($countryId) {
+            CountryId::CZ => $this->validateCz($value),
+            default => throw new InvalidArgumentException('Unsupported country ID'),
+        };
+    }
+
+    private function validateCz(string $value): bool
+    {
+        // be liberal in what you receive
+        $ic = preg_replace('#\s+#', '', $value);
+
+        // is it the desired format?
+        if (!preg_match('#^\d{8}$#', $ic)) {
+            return false;
+        }
+
+        // checksum
+        $a = 0;
+        for ($i = 0; $i < 7; $i++) {
+            $a += $ic[$i] * (8 - $i);
+        }
+
+        $a %= 11;
+        if ($a === 0) {
+            $c = 1;
+        } elseif ($a === 1) {
+            $c = 0;
+        } else {
+            $c = 11 - $a;
+        }
+
+        return (int) $ic[7] === $c;
+    }
+}
---
+++ src/Validation/PersonalIdentificationNumberValidator.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Sovic\Common\Validation;
+
+use InvalidArgumentException;
+use Sovic\Common\Entity\Enum\CountryId;
+
+class PersonalIdentificationNumberValidator
+{
+    public function validate(string $value, CountryId $countryId): bool
+    {
+        if ($countryId === CountryId::CZ || $countryId === CountryId::SK) {
+            return $this->validateCzSk($value);
+        }
+
+        throw new InvalidArgumentException('Unsupported country ID');
+    }
+
+    // CZ, SK rodné číslo
+    private function validateCzSk(string $value): bool
+    {
+        // be liberal in what you receive
+        if (!preg_match('#^\s*(\d\d)(\d\d)(\d\d)[ /]*(\d\d\d)(\d?)\s*$#', $value, $matches)) {
+            return false;
+        }
+
+        [, $year, $month, $day, $ext, $c] = $matches;
+
+        if ($c === '') {
+            $year += $year < 54 ? 1900 : 1800;
+        } else {
+            // checksum
+            $mod = ($year . $month . $day . $ext) % 11;
+            if ($mod === 10) {
+                $mod = 0;
+            }
+            if ($mod !== (int) $c) {
+                return false;
+            }
+
+            $year += $year < 54 ? 2000 : 1900;
+        }
+
+        // 20, 50 or 70 can be added to the month
+        if ($month > 70 && $year > 2003) {
+            $month -= 70;
+        } elseif ($month > 50) {
+            $month -= 50;
+        } elseif ($month > 20 && $year > 2003) {
+            $month -= 20;
+        }
+
+        // data check
+        if (!checkdate($month, $day, $year)) {
+            return false;
+        }
+
+        return true;
+    }
+}
