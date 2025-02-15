82ccd6b4d2fbf454b90097e1acad1c090c2609eb
Vojtěch Bojda <info@vojtechbojda.cz>
2025-02-15 22:08:23 +0100

    adds SettingGroupId

--- src/Project/SettingGroupId.php
+++ src/Project/SettingGroupId.php
@@ -5,6 +5,10 @@ namespace Sovic\Common\Project;
 enum SettingGroupId: string
 {
     case App = 'app';
+    case External = 'external';
+    case Gallery = 'gallery';
     case Mailer = 'mailer';
+    case Routing = 'routing';
     case Security = 'security';
+    case Theme = 'theme';
 }
