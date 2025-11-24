fb3940963a2817b0a4067f8182a8c308240fd6af
Vojtěch Bojda <info@vojtechbojda.cz>
2025-11-24 22:06:36 +0100

    adds SettingGroupId:trans

--- src/Project/SettingGroupId.php
+++ src/Project/SettingGroupId.php
@@ -11,4 +11,17 @@ enum SettingGroupId: string
     case Routing = 'routing';
     case Security = 'security';
     case Theme = 'theme';
+
+    public function trans(): string
+    {
+        return match ($this) {
+            self::App => 'Aplikace',
+            self::External => 'Externí služby',
+            self::Gallery => 'Galerie',
+            self::Mailer => 'Emaily',
+            self::Routing => 'Routing',
+            self::Security => 'Zabezpečení',
+            self::Theme => 'Vzhled',
+        };
+    }
 }
